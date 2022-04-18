0ba1fb89a7318e02aeeb805399141b52c0572e8a
2000egores@mail.ru <c3a8v00c3a8v00>
2022-04-18 21:33:12 +0300

    Com

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import atexit
 import json
 import os
 
-from cloudant.client import Cloudant
+from cloudant import Cloudant
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='')
